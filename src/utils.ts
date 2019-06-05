5911c137cc3ba07f59adf585c4ff95579c39ed6e
Marta Sušek <m.susek@live.com>
2019-06-05 21:02:52 +0200

    Add utils

---
+++ src/utils.ts
@@ -0,0 +1,40 @@
+
+import fs from "fs"
+/**
+ * @param  {string} data string in format hh:mm:ss
+ * @returns {number} duration in miliseconds
+ */
+export function getDuration(data: string): number {
+    let dataArr = data.split(":")
+    if (dataArr.length !== 3)
+        throw new Error("Invalid time format")
+    let duration = 0
+    dataArr.forEach((d: string, i: number) => {
+        switch (i) {
+            case (0): duration += parseInt(d) * 3600 * 1000
+                break;
+            case (1): duration += parseInt(d) * 60 * 1000
+                break;
+            case (2): duration += parseInt(d) * 1000
+
+        }
+    })
+    return duration
+}
+/**
+ * @param  {string} dir directory path
+ */
+export async function checkDir(dir: string) {
+    return new Promise((resolve, reject) => {
+        if (!fs.existsSync(dir))
+            return fs.mkdir(dir, (err) => {
+                if (err) {
+                    reject(err)
+                    return
+                }
+                resolve()
+            })
+        else
+            resolve()
+    })
+}
