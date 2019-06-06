b472b8e562cd8e2661ea55266b246ad2a46d578a
Marta Sušek <m.susek@live.com>
2019-06-06 18:02:19 +0200

    Add more tests

--- tests/e2e.test.ts
+++ tests/e2e.test.ts
@@ -3,16 +3,17 @@ import fs from "fs"
 import Instances from "../src/instances";
 jest.setTimeout(60000)
 
-
+const VIDEO_URL="https://www.youtube.com/watch?v=668nUCeBHyY"
+const VIDEO_FILENAME="Nature Beautiful short video 720p HD-668nUCeBHyY.mp4"
 describe("End to end", () => {
     const instances = new Instances()
-    let filename: string
+    let filename:string
     test('Get video info', async () => {
-        filename = await instances.downloader.getVideoInfo("https://www.youtube.com/watch\?v\=UxPTGk_uzPU").then(vi => vi._filename)
-        expect(filename).toBe("E.T - Tek Je 12 Sati-UxPTGk_uzPU.mp4")
+        filename = await instances.downloader.getVideoInfo(VIDEO_URL).then(vi => vi._filename)
+        expect(filename).toBe(VIDEO_FILENAME)
     })
     test('Download video', async () => {
-        await new Youcon().init(["https://www.youtube.com/watch\?v\=UxPTGk_uzPU"], "./test_files")
+        await new Youcon().init([VIDEO_URL], "./test_files")
         const e = fs.existsSync(`./test_files/${filename}`)
         expect(e).toBe(true)
     });
@@ -20,7 +21,7 @@ describe("End to end", () => {
         const file = filename.split(".")
         file.pop()
         const mp3File = file.join(".") + ".mp3"
-        await new Youcon().init(["https://www.youtube.com/watch\?v\=UxPTGk_uzPU"], "./test_files", true)
+        await new Youcon().init([VIDEO_URL], "./test_files", true)
         const e = fs.existsSync(`./test_files/mp3/${mp3File}`)
         expect(e).toBe(true)
     });
@@ -28,7 +29,7 @@ describe("End to end", () => {
         const file = filename.split(".")
         file.pop()
         const aviFile = file.join(".") + ".avi"
-        await new Youcon().init(["https://www.youtube.com/watch\?v\=UxPTGk_uzPU"], "./test_files", true, "avi")
+        await new Youcon().init([VIDEO_URL], "./test_files", true, "avi")
         const e = fs.existsSync(`./test_files/avi/${aviFile}`)
         expect(e).toBe(true)
     });
