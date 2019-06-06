ad830f15e58e0d96e74ddeacbb80bfeefeef87f5
Marta Sušek <m.susek@live.com>
2019-06-06 18:02:52 +0200

    Extend writeStream interface

---
+++ types/process.d.ts
@@ -0,0 +1,10 @@
+import { Readable } from "stream";
+
+export default interface ProcessExec extends NodeJS.Process {
+    stdout: StdOutExec
+}
+
+interface StdOutExec extends NodeJS.WriteStream {
+    cursorTo?: (l: number) => void
+    clearLine?: (l: number) => void
+}
