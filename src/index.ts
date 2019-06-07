9e846a55a735d4cecebe411b580fa23aa17a2a93
Marta Sušek <m.susek@live.com>
2019-06-07 16:46:00 +0200

    Additional error handling

--- src/index.ts
+++ src/index.ts
@@ -24,9 +24,13 @@ export default class Youcon {
      */
     async init(urls?: Array<string>, dir?: string, convert?: boolean, format?: string) {
         this._args.setArgs(urls, dir, convert, format)
+        if (!Array.isArray(this._args.urls)) {
+            throw new Error("The first argument must be an array!")
+        }
         process.stdout.write(`Downloading ${this._args.urls.length} videos to directory ${this._args.dir}`)
+
         for (let url of this._args.urls)
-            await this._downloader.downloadVideos(url.toString(), this._args.dir)
+            await this._downloader.downloadVideos(url.toString(), this._args.dir).catch(err => { throw err })
     }
 }
 
