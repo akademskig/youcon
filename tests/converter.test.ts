68c832227da6568a6f391520e8300b0b765c197d
Marta Sušek <m.susek@live.com>
2019-06-10 19:54:47 +0200

    Update tests

--- tests/converter.test.ts
+++ tests/converter.test.ts
@@ -1,26 +1,37 @@
 import fs from "fs"
 import Instances from "../src/instances";
+import path from "path"
 jest.setTimeout(60000)
 
 
 describe("Converter class", () => {
     const instances = new Instances()
     let filename = "test_file.mp4"
-    test('Convert a file to .mp3', async () => {
-        await instances.converter.convert(filename, "./test_files", "mp3")
-        const file = filename.split(".")
-        file.pop()
-        const mp3File = file.join(".") + ".mp3"
-        const e = fs.existsSync(`./test_files/mp3/${mp3File}`)
-        expect(e).toBe(true)
-    })
-    test('Convert a file to .avi', async () => {
-        await instances.converter.convert(filename, "./test_files", "avi")
-        const file = filename.split(".")
-        file.pop()
-        const mp3File = file.join(".") + ".avi"
-        const e = fs.existsSync(`./test_files/avi/${mp3File}`)
-        expect(e).toBe(true)
-    })
 
+    describe("Download ffmpeg", () => {
+        test('Download ffmpeg', async () => {
+            await instances.updater.init()
+            const ffPath = JSON.parse((fs.readFileSync(path.join(__dirname,"../src/ffmpeg-downloader/ffpath.json"), { encoding: "utf8" }).toString()))
+            const e = fs.existsSync(ffPath.ffPath)
+            expect(e).toBe(true)
+        })
+    })
+    describe("Convert files", () => {
+        test('Convert a file to .mp3', async () => {
+            await instances.converter.convert(filename, "./test_files", "mp3")
+            const file = filename.split(".")
+            file.pop()
+            const mp3File = file.join(".") + ".mp3"
+            const e = fs.existsSync(`./test_files/mp3/${mp3File}`)
+            expect(e).toBe(true)
+        })
+        test('Convert a file to .avi', async () => {
+            await instances.converter.convert(filename, "./test_files", "avi")
+            const file = filename.split(".")
+            file.pop()
+            const mp3File = file.join(".") + ".avi"
+            const e = fs.existsSync(`./test_files/avi/${mp3File}`)
+            expect(e).toBe(true)
+        })
+    })
 })
--- tests/e2e.test.ts
+++ tests/e2e.test.ts
@@ -1,13 +1,13 @@
 import Youcon from "../src/index"
 import fs from "fs"
 import Instances from "../src/instances";
-jest.setTimeout(60000)
+jest.setTimeout(100000)
 
-const VIDEO_URL="https://www.youtube.com/watch?v=668nUCeBHyY"
-const VIDEO_FILENAME="Nature Beautiful short video 720p HD-668nUCeBHyY.mp4"
+const VIDEO_URL = "https://www.youtube.com/watch?v=668nUCeBHyY"
+const VIDEO_FILENAME = "Nature Beautiful short video 720p HD-668nUCeBHyY.mp4"
 describe("End to end", () => {
     const instances = new Instances()
-    let filename:string
+    let filename: string
     test('Get video info', async () => {
         filename = await instances.downloader.getVideoInfo(VIDEO_URL).then(vi => vi._filename)
         expect(filename).toBe(VIDEO_FILENAME)
@@ -17,20 +17,23 @@ describe("End to end", () => {
         const e = fs.existsSync(`./test_files/${filename}`)
         expect(e).toBe(true)
     });
-    test('Convert video to .mp3', async () => {
-        const file = filename.split(".")
-        file.pop()
-        const mp3File = file.join(".") + ".mp3"
-        await new Youcon().init([VIDEO_URL], "./test_files", true)
-        const e = fs.existsSync(`./test_files/mp3/${mp3File}`)
-        expect(e).toBe(true)
-    });
-    test('Convert video to .avi', async () => {
-        const file = filename.split(".")
-        file.pop()
-        const aviFile = file.join(".") + ".avi"
-        await new Youcon().init([VIDEO_URL], "./test_files", true, "avi")
-        const e = fs.existsSync(`./test_files/avi/${aviFile}`)
-        expect(e).toBe(true)
-    });
+
+    describe("Convert videos", () => {
+        test('Convert video to .mp3', async () => {
+            const file = filename.split(".")
+            file.pop()
+            const mp3File = file.join(".") + ".mp3"
+            await new Youcon().init([VIDEO_URL], "./test_files", true)
+            const e = fs.existsSync(`./test_files/mp3/${mp3File}`)
+            expect(e).toBe(true)
+        });
+        test('Convert video to .avi', async () => {
+            const file = filename.split(".")
+            file.pop()
+            const aviFile = file.join(".") + ".avi"
+            await new Youcon().init([VIDEO_URL], "./test_files", true, "avi")
+            const e = fs.existsSync(`./test_files/avi/${aviFile}`)
+            expect(e).toBe(true)
+        });
+    })
 })
